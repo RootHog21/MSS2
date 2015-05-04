e6f14ae826d30e7ccb3326c994f27b9f769ade6a
ims1993 <strnadtraffic@gmail.com>
2015-05-04 14:33:03 -0500

    some more fixes
    deleted unused main and fixed security java

--- app/src/main/java/omicron/mss/SecurityActivity.java
+++ app/src/main/java/omicron/mss/SecurityActivity.java
@@ -11,14 +11,14 @@ public class SecurityActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_security);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_security, menu);
         return true;
     }
 
