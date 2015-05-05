2558697e7b4db4264c46e55b6f20c9b1c9b5e6e7
ims1993 <strnadtraffic@gmail.com>
2015-05-05 17:10:16 -0500

    More activities
    changed more things to activities

---
+++ app/src/main/java/omicron/mss/AddClassActivity.java
@@ -0,0 +1,53 @@
+package omicron.mss;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class AddClassActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_class);
+    }
+
+    //called when user clicks Back
+    public void backToCreate(View view) {
+        Intent createIntent = new Intent(this, CreateScheduleActivity.class);
+        startActivity(createIntent);
+    }
+
+    //called when user clicks Add Class
+    public void addClass(View view) {
+        //add add class algorithm
+        Intent createIntent = new Intent(this, CreateScheduleActivity.class);
+        startActivity(createIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_class, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
