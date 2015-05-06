f0787e15fb3a38d5db3b1dd306ec25f468a1e38a
ims1993 <strnadtraffic@gmail.com>
2015-05-06 14:57:08 -0500

    Schedule Creator
    beta thing i guess

--- app/src/main/java/omicron/mss/ScheduleCreator.java
+++ app/src/main/java/omicron/mss/ScheduleCreator.java
@@ -1,16 +1,41 @@
 package omicron.mss;
 
+
+
+import android.util.Log;
+
+import com.parse.*;
+
+import java.util.List;
+
+
+
 /**
  * Created by Ian on 5/4/2015.
  */
 public class ScheduleCreator {
 
-    public ScheduleCreator(){
+    public ScheduleCreator() {
         //can't finish yet
     }
 
+    public void findClass() throws ParseException {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("ClassDB");
+        List<ParseObject> classList;
+        query.whereEqualTo("classNum", "4393");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> classList, ParseException e) {
+                if (e == null) {
+                    Log.d("class", "retrieved " + classList.size() + " scores");
+                } else {
+                    Log.d("class", "Error: " + e.getMessage());
+                }
+            }
+        });
+        System.out.println(query.get("CziY9WboXS"));
+    }
 
     void compare(String time1, String time2){
-        
+
     }
 }
